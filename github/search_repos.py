a930e1b716204a78ffd0e8d51f6bb71f905eaecf
Eric Hambro <eric.hambro@gmail.com>
2018-01-17 21:16:17 +0000

    collection of repo urls done

--- github/search_repos.py
+++ github/search_repos.py
@@ -5,15 +5,14 @@ import json
 import pprint
 import shutil
 import argparse
-
-import os.path
+import os
 
 from collections import namedtuple
 
 GITHUB = 'https://api.github.com'
 AUTH = None
 OFFLINE = False
-PP = pprint.PrettyPrinter(indent=4, depth=2)
+PP = pprint.PrettyPrinter(indent=4, depth=2) # for debugging
 
 SAVE_FILE = "urls.save.json"
 CACHE_FILE = "repo_req.cache.json"
@@ -28,7 +27,7 @@ def parse_args():
     parser.add_argument('-l', '--language', default='python', dest='language', 
                         nargs=1, action='store',
                         help='language of the repo (according to GH)')
-    parser.add_argument('-p', '--pages', default=10, type=int, nargs=1,
+    parser.add_argument('-p', '--pages', default=4, type=int, 
                     help='how many pages of results (100 per page)')
     return vars(parser.parse_args())
 
@@ -79,6 +78,7 @@ def quick_save(name, query, query_url, urls, page):
         g.write(json.dumps(js, sort_keys=True, indent=4, separators=(',', ': ')))
     
     shutil.copy(bk_file, SAVE_FILE)
+    os.remove(bk_file)
 
 def query_github(end, payload):
     '''
@@ -91,11 +91,11 @@ def query_github(end, payload):
         return load_cache()
     return requests.get(GITHUB + end, auth=AUTH["github"], params=payload)
     
-def make_repository_search_request(terms, language, page):  
+def make_code_search_request(terms, language, page):  
     '''
     Create request and endpoint to send to GitHub
     '''
-    end = "/search/repositories"
+    end = "/search/code"
     filters = " language:{} fork:false".format(language)
 
     payload = {
@@ -113,12 +113,12 @@ def extract_repo_data(repo_request_json):
     '''
     repos = []
     for raw_repo in repo_request_json["items"]:
+        # PP.pprint(raw_repo)  # debug
         r = {
-            'url': raw_repo["clone_url"],
-            'name': raw_repo["name"],
-            'id': raw_repo['id'],
-            'size': raw_repo['size'],
-            'stars': raw_repo["stargazers_count"],
+            'url': raw_repo["repository"]["html_url"],
+            'name': raw_repo["repository"]["full_name"],
+            'id': raw_repo["repository"]['id'],
+            'score': raw_repo['score'],
             'searched': False
         }
         repos.append(r)
@@ -128,8 +128,8 @@ def search_repos(search_terms, language, pages):
     '''
     Conduct searches for repositories and save the data. 
     '''
-    for p in range(pages):
-        req = make_repository_search_request(search_terms, language, p)
+    for p in range(1, pages+1): # GH page from 1 not 0
+        req = make_code_search_request(search_terms, language, p)
         res = query_github(*req)
 
         print("Request {}: {}: RC: {}".format(p, res.url, res.status_code))
@@ -142,7 +142,7 @@ def search_repos(search_terms, language, pages):
 
 
 def main(search_terms, language, pages):
-    print("Searching for {} {} pages of : {} ".format(
+    print("Searching GH code for {} {} pages with: {} ".format(
         pages, language, " ".join(search_terms)))
     search_repos(search_terms, language, pages)
 
