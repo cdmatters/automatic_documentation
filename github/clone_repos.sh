a980e1a7e13e8de44da805c2ea310a7e2caca800
Eric Hambro <eric.hambro@gmail.com>
2018-03-02 17:00:35 +0000

    safe mode

--- github/clone_repos.sh
+++ github/clone_repos.sh
@@ -1,17 +1,17 @@
 function line_em_up (){
   mkdir -p data/ subcmds/
   grep "\"url\":" $1 | uniq | sed "s/^.*\"url\": /git clone --depth=2 --shallow-submodules /" | \
-      sed  "s/github\.com\/\(.*\)\/\(.*\)\"/github.com\/\1\/\2\"  \.\/data\/\1__-__\2  && \\/" > subcmds_master
+      sed  "s/github\.com\/\(.*\)\/\(.*\)\"/github\.com\/\1\/\2\"  \.\/data\/\1__-__\2  \&\& \\\\/" > subcmds_master
 
   let "b=$(wc -l < subcmds_master)/$2"
   split -l $b subcmds_master subcmds/sub_bash_
   chmod 755 subcmds/sub_bash_*
-
 }
 
 function clone_em_all (){
   for x in $(ls subcmds); do
     echo "Executing $x"
+    echo "echo 'done'" >> subcmds/$x
     ./subcmds/$x &
   done
 }
