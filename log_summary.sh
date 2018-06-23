580329ab727ce5f01d047c7c371ea61592dbad42
Eric Hambro <eric.hambro@gmail.com>
2018-06-23 19:51:36 +0100

    added log summary

---
+++ log_summary.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function log_summary(){
+    
+  blink=$(tput setaf blink)
+  green=$(tput setaf 2)
+  black=$(tput setaf 0)
+  yellow=$(tput setaf 3)
+  blue=$(tput setaf 6)
+  bold=$(tput bold)
+  reset=$(tput sgr0)
+
+
+  if [ $1 = "-f" ]; then
+    shift 1
+    func="tail -f"
+    echo $@
+  else
+    func="cat"
+  fi
+
+  $func $@ | \
+    sed -E -e "s/ARGN:(.*)<END>/ARGN:$bold\1$reset<END>/" -e \
+              "s/(ARGN:|DESC:|INFR:)/$bold$green\1$reset/" -e \
+              "s/(MINIBATCHES:|TRAIN_BLEU:|TEST_BLEU:)/$bold\1$reset/" -e \
+              "s/(----*)/$bold$black\1$reset/" -e \
+              "s/(TRAINING:.*)/$bold$blue\1$reset/" -e \
+              "s/(TEST:.*)/$bold$yellow\1$reset/" 
+}
+
+log_summary $@
