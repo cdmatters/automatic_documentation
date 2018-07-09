609de26e223cc5238a9a181e957d0b2b51d335f7
Eric Hambro <eric.hambro@gmail.com>
2018-07-09 20:29:53 +0100

    get adaptive vocab

--- project/utils/nmt_util.py
+++ project/utils/nmt_util.py
@@ -1,11 +1,25 @@
 from collections import Counter
+import os
 
+import numpy as np
 from project.utils.tokenize import get_data_tuple, choose_tokenizer, \
                      CHAR_VOCAB, nltk_tok,\
                      get_embed_filenames, get_weights_word2idx, \
                      get_weights_char2idx
 
 NMT_PATH = "./nmt/nmt_data/{}"
+
+def gen_embed_file(dim):
+    DIR = os.path.dirname(os.path.abspath(__file__))
+
+    with open(get_embed_filenames()[dim], 'r') as f:
+        lines = f.readlines()
+    start_toks = ["<unk>", "<s>", "</s>"]
+    start_lines = ["{} {}\n".format(t, " ".join(["{:5f}".format(i) for i in np.random.randn(dim)])) for t in start_toks]
+    start_lines.extend(lines)
+    with open('{}/glove_inserted.{}.txt.de'.format(DIR, dim), 'w') as f:
+        for s in start_lines:
+            f.write(s)
 
 def write_data_to_file(data, name):
     char_seq = []
@@ -65,9 +79,11 @@ def gen_desc_vocab_file(train_data, vocab_size, dim):
 
 
 if __name__ == "__main__":
-    desc_embed=300
-    char_embed = 300
-    vocab_size=10000
+    # gen_embed_file(200)
+
+    desc_embed = 200
+    char_embed = 200
+    vocab_size=35000
 
     data_tuple = get_data_tuple(use_full_dataset=True, use_split_dataset=False)
     tokenizer = choose_tokenizer('var_only')
@@ -78,7 +94,7 @@ if __name__ == "__main__":
     gen_desc_vocab_file(data_tuple.train, vocab_size, desc_embed)
 
     print("Loading GloVe weights and word to index lookup table")
-    word_weights, word2idx = get_weights_word2idx(desc_embed, vocab_size)
+    word_weights, word2idx = get_weights_word2idx(desc_embed, vocab_size, data_tuple.train)
     print("Creating char to index look up table")
     char_weights, char2idx = get_weights_char2idx(char_embed)
 
