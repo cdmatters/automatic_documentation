3b16b2fb1d735edf8692015d4f6c737c2f085eeb
Eric Hambro <eric.hambro@gmail.com>
2018-08-06 17:11:22 -0400

    70dim max

--- project/utils/args.py
+++ project/utils/args.py
@@ -14,9 +14,9 @@ def data_args(parse_fn):
         p.add_argument('--desc-seq', '-d', dest='desc_seq', action='store',
                        type=int, default=120,
                        help='max desecription sequence length')
-        p.add_argument('--char-embed', '-f', dest='char_embed', action='store',
-                       type=int, default=200,
-                       help='size of char embedding')
+        #p.add_argument('--char-embed', '-f', dest='char_embed', action='store',
+        #               type=int, default=200,
+        #               help='size of char embedding')
         p.add_argument('--desc-embed', '-g', dest='desc_embed', action='store',
                        type=int, default=200,
                        help='size of glove embedding: 50, 100, 200 or 300')
